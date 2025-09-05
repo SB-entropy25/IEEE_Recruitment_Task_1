bf8448d68f587e78cede3bc1f84f5851b1da7dbe
SB-entropy25 <srijan2shourya@gmail.com>
2025-09-05 22:35:04 +0530

    Update Question_1.py

--- Question_1.py
+++ Question_1.py
@@ -1,3 +1,4 @@
-n = int(input("Enter the value of n:"))
-for i in range(1, n+1):
+n = int(input("Enter the value of n:")) #we take input from user regarding value of number of rows
+for i in range(1, n+1):        #in 1st line there are n-1 blanks
     print(' ' * (n-i) + '*' * i)
+
